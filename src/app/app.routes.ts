d31990a8f90d990f7823a0656b0215e2502c62d8
aashinsiby <aashin2000@gmail.com>
2024-05-02 15:47:32 +0530

    1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { VisionComponent } from './vision/vision.component';
+import { ProgramsComponent } from './programs/programs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', redirectTo: 'home', pathMatch:"full"},
+   { path: 'home', component: HomeComponent},
+  {path: 'contact', component: ContactComponent},
+  { path: 'vision', component: VisionComponent},
+  { path: 'programs', component: ProgramsComponent},
+   { path: '**', component: HomeComponent } // Wildcard route for any unmatched routes
+  
+  ];
