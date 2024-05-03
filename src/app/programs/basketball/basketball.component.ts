c1f8b9ed4e4cd664c3566b665d0181b86ec64219
aashinsiby <aashin2000@gmail.com>
2024-05-03 15:59:54 +0530

    13

--- src/app/programs/basketball/basketball.component.ts
+++ src/app/programs/basketball/basketball.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-basketball',
   standalone: true,
-  imports: [],
+  imports: [MatExpansionModule,MatIconModule],
   templateUrl: './basketball.component.html',
   styleUrl: './basketball.component.css'
 })
