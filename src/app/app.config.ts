63545766fbb7c862b6d57c0c51bd483db2189814
aashinsiby <aashin2000@gmail.com>
2024-05-03 23:57:18 +0530

    20

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), 
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"apexprime-243f7","appId":"1:429676747094:web:c5a63145e1068811875ef7","databaseURL":"https://apexprime-243f7-default-rtdb.asia-southeast1.firebasedatabase.app","storageBucket":"apexprime-243f7.appspot.com","apiKey":"AIzaSyChBFEEJYjRhIDe_DznwY1_f2piJSAA1gs","authDomain":"apexprime-243f7.firebaseapp.com","messagingSenderId":"429676747094"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase()))
+  ]
+
 };
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { Database, get, onValue, ref, set, update } from '@angular/fire/database';
+import { FormControl,FormsModule,  FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  messageform = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    school: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    message: new FormControl('', [Validators.required]),
+  
+  });
+constructor(private database: Database){
+
+}
+sendmessage(formValues: any){
+
+  if (formValues.name === '') {
+    alert('Please enter name');
+    return;
+  }
+  if (formValues.school === '') {
+    alert('Please enter School/College Name');
+    return;
+  }
+  if (formValues.phon === '') {
+    alert('Please enter Phone Number / email');
+    return;
+  }
+  set(ref(this.database,'users/'+ formValues.name),{
+    phone : formValues.phone,
+    name : formValues.name,
+    school : formValues.school,
+    message : formValues.message,
+ 
+
+  })
+  alert('Message send Succesfully');
+  const starCountRef = ref(this.database,'users/'+formValues.name)
+          
+  return onValue(starCountRef, (snapshot) => {
+    const username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+  
+  }, {
+    onlyOnce: true
+  });
+ 
+
+}
 
 }
