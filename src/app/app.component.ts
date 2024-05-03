478c7363269e6ab73bb83f74f56ec2be0caef362
aashinsiby <aashin2000@gmail.com>
2024-05-03 19:53:49 +0530

    14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterOutlet,MatIconModule,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'apexprime';
+  scrolled = false;
+  lastScrollY: number = 0;
+constructor(private router :Router){}
+  
+home(){
+  this.router.navigate(['/home']);
+}
+@HostListener('window:scroll', ['$event'])
+onWindowScroll() {
+  const currentScrollY = window.pageYOffset;
+
+  if (currentScrollY === 0) {
+    
+    this.scrolled = false;
+  } 
+  else if (currentScrollY > this.lastScrollY) {
+
+    this.scrolled = true;
+  } 
+  else{
+    
+    this.scrolled = true;
+  }
+
+  this.lastScrollY = currentScrollY;
+}
+  scrol(){
+    this.scrolled = false;
+
+  }
 }
